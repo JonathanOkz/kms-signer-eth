47b02b4b01decd56117532b5daf23aee96a4ad7b
Jonathan Okz <jonathanolesz@gmail.com>
2023-02-07 22:54:37 +0100

    add custom chain support

--- src/Signer.ts
+++ src/Signer.ts
@@ -1,24 +1,26 @@
-import { KMS } from './Types/KMS';
+import { addHexPrefix } from '@ethereumjs/util'
 import { Transaction, TxData } from '@ethereumjs/tx';
 import { Common } from '@ethereumjs/common'
+import { KMS } from './Types/KMS';
 import { Account } from './Account';
 
 export class Signer {
     kms: KMS;
-    chainId?: number;
     common?: Common;
 
     constructor(kms: KMS, chainId?: number) {
         this.kms = kms;
-        this.chainId = chainId;
-        this.common = (chainId) ? new Common({ chain: chainId }) : undefined;
+        this.common = (chainId) ? Common.custom({ chainId: chainId, networkId: chainId }) : undefined;
     }
 
+     /**
+     * @returns The tnx serialized ECDSA signature as a '0x'-prefixed string.
+     */
     public async signTransaction(account: Account | { address: Buffer, KeyId: string }, txData: TxData) {
-        const unsignedTx = Transaction.fromTxData(txData, { common: this.common }).getMessageToSign();
-        const {r, s, v}  = await this.kms.ecsign(account.address, account.KeyId, unsignedTx, this.chainId);
-        const signedTx   = Transaction.fromTxData({...txData, r, s, v}, { common: this.common });
+        const digest     = Transaction.fromTxData(txData, { common: this.common }).getMessageToSign();
+        const {r, s, v}  = await this.kms.ecsign(account.address, account.KeyId, digest, this.common?.chainId());
+        const signed     = Transaction.fromTxData({...txData, r, s, v}, { common: this.common });
     
-        return `0x${signedTx.serialize().toString('hex')}`;
+        return addHexPrefix(signed.serialize().toString('hex'));
     }
 }
